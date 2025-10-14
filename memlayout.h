e28c402e831f904a5050b3c480e73160b66d9990
aasthaaj05 <aastha.anuj.jajoo@gmail.com>
2025-10-14 12:07:47 +0530

    added HEAPLIMIT, only declarations of SYSCALLS

--- memlayout.h
+++ memlayout.h
@@ -6,6 +6,7 @@
 
 // Key addresses for address space layout (see kmap in vm.c for layout)
 #define KERNBASE 0x80000000         // First kernel virtual address
+#define HEAPLIMIT 0x70000000	    // heap ends here, shared mem starts	  
 #define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
 
 #define V2P(a) (((uint) (a)) - KERNBASE)
