ab95509482cad69a72162a9e3de694430393cd51
akanshapatil25 <akapatil25@gmail.com>
2025-10-23 20:06:19 +0530

    Updated shm.h

--- shm.h
+++ shm.h
@@ -4,3 +4,19 @@
 #define SHMMIN    1               //1 byte min segment size
 #define MAXSHM    64              //max 64 segments
 #define SHMSEG    MAXSHM          //max segments per process
+#define MAX_PAGES 16
+
+struct shmseg{
+    int used;
+    int key;
+    int id;
+    int size;
+    int nattch;
+    char *pages[MAX_PAGES];
+};
+
+extern struct shmseg shmtable[MAXSHM];
+void shminit(void);
+int shmget(int key, int size);
+
+
